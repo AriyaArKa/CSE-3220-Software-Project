db43bbff9822f3edc985883af0bdde7a8a9daa61
arkanath <natharka55@gmail.com>
2026-03-20 18:56:34 +0600

    Initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ SoftwareProjectHexashop/src/main/java/com/example/SoftwareProjectHexashop/SoftwareProjectHexashopApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoftwareProjectHexashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProjectHexashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareProjectHexashopApplication.class, args);
+	}
+
+}
---
+++ SoftwareProjectHexashop/src/test/java/com/example/SoftwareProjectHexashop/SoftwareProjectHexashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoftwareProjectHexashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareProjectHexashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
